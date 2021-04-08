3b89e7f5beef40b9fdf93423a71f625ec99ce3b8
ankush-sheoran-coder <82148279+ankush-sheoran-coder@users.noreply.github.com>
2021-04-08 15:37:21 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,4 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','vss_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('fc7b0efa8082.ngrok.io','root','','vss_db')or die("Could not connect to mysql".mysqli_error($con));
+?>
